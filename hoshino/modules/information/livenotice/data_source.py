ee7fe57ef3f439f99d294811fe67c14622991b3d
shewinder <2813349544@qq.com>
2021-04-21 03:29:36 +0800

    Merge branch 'main' of https://gitee.com/luo_zhen_xu/shebot_nb2 into main

---
+++ hoshino/modules/information/livenotice/data_source.py
@@ -0,0 +1,254 @@
+import asyncio
+import json
+import os
+import abc
+from typing import List
+
+import aiohttp
+import peewee as pw
+import requests
+
+from hoshino.log import logger
+from hoshino import db_dir, Bot, get_bot_list
+
+PROXY_POOL_URL = 'http://140.143.122.138:5555/random'
+
+def get_proxy():
+    try:
+        response = requests.get(PROXY_POOL_URL)
+        if response.status_code == 200:
+            return response.text
+    except ConnectionError:
+        return None
+
+
+db_path = os.path.join(db_dir, 'live.db')
+db = pw.SqliteDatabase(db_path)
+
+class SubscribedLive(pw.Model):
+    platform = pw.TextField()
+    room_id = pw.TextField()
+    name = pw.TextField()
+    date = pw.TextField()
+    groups = pw.TextField()
+
+    class Meta:
+        database = db
+        primary_key = pw.CompositeKey('platform', 'room_id')
+
+if not os.path.exists(db_path):
+    db.connect()
+    db.create_tables([SubscribedLive])
+    db.close()
+
+class Live:
+    @property
+    def platform(self):
+        raise NotImplementedError
+
+    def check_update():
+        raise NotImplementedError
+
+    def last_update():
+        raise NotImplementedError
+
+    @staticmethod
+    async def notice(sub: SubscribedLive, title: str, url: str):
+        bot: Bot = get_bot_list()[0]
+        groups = sub.groups.split(',')
+        for gid in groups:
+            gid = int(gid)
+            try:
+                await bot.send_group_msg(group_id=gid, message=f'{sub.name}开播啦\n{title}\n{url}')
+            except Exception as e:
+                logger.exception(e)
+            await asyncio.sleep(0.5)
+
+    @staticmethod
+    def get_group_subscrib(group_id: int) -> List[SubscribedLive]:
+        sql = pw.SQL('groups like ?', params=[f'%{group_id}%'])
+        return SubscribedLive.select().where(sql)
+
+    @staticmethod
+    def delete_group_live(group_id: int, sub: SubscribedLive):
+        groups: List[str] = sub.groups.split(',')
+        if str(group_id) in groups:
+            groups.remove(str(group_id))
+            sub.groups = ','.join(groups)
+            sub.save()
+        if not groups:
+            sub.delete_instance()
+        
+        
+
+
+class BiliBiliLive(Live):
+    api_url = 'https://api.live.bilibili.com/room/v1/Room/get_info'
+
+    @property
+    def platform():
+        return 'bilibili'
+    
+    @classmethod
+    async def get_name_from_room(cls, room_id: int) -> dict:
+        headers = {
+            'Referer': 'https://link.bilibili.com/p/center/index',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        }
+        params = {
+            'roomid': room_id
+        } 
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get('https://api.live.bilibili.com/live_user/v1/UserInfo/get_anchor_in_room', 
+                                        headers=headers, 
+                                        params=params) as resp:
+                    if resp.status == 200:
+                        json_dic = await resp.json()
+                        return json_dic['data']['info']['uname']
+                    else:
+                        logger.warning(f'未能成功获取主播名')
+                        return 'unknown'
+            except Exception as e:
+                logger.exception(e)
+                return 'unknown'
+
+    @classmethod
+    async def _get_bilibili_live_info(cls, room_id: int, use_proxy=False) -> dict:
+        proxy = 'http://' + get_proxy() if use_proxy else None
+        headers = {
+            'Referer': 'https://link.bilibili.com/p/center/index',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        }
+        params = {
+            'room_id': room_id
+        }
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(cls.api_url, headers=headers, params=params, proxy=proxy) as resp:
+                    if resp.status == 200:
+                        json_dic = await resp.json()
+                        return json_dic
+                    else:
+                        logger.warning(f'访问B站直播发生错误，错误码{resp.status}')
+            except Exception as e:
+                #logger.exception(e)
+                return {}
+
+    async def check_update(self):
+        # 根据直播时间检查所有直播间是否更新
+        subs: List[SubscribedLive] = SubscribedLive.select().where(SubscribedLive.platform == 'bilibili')
+        if not subs:
+            logger.info('检查直播： 当前未订阅直播')
+            return
+        for sub in subs:
+            last_date = sub.date
+            resp = await self._get_bilibili_live_info(sub.room_id, use_proxy=True)
+            if not resp:
+                logger.warning(f'检查B站直播间{sub.room_id}出错')
+                continue
+            if resp['code'] != 0:
+                logger.warning(f'检查B站直播间{sub.room_id}: {resp["message"]}')
+                continue
+            data = resp['data']
+            if last_date != data['live_time'] and data['live_status'] == 1:
+                logger.info(f'检测到B站直播间{sub.room_id}更新')
+                sub.date = data['live_time']
+                sub.save()
+                await self.notice(sub, data['title'], f'https://live.bilibili.com/{sub.room_id}')
+    
+    @classmethod
+    async def check_room_exists(cls, room_id: int) -> bool:
+        resp = await cls._get_bilibili_live_info(room_id)
+        if resp:
+            if resp['code'] == 0:
+                return True
+        return False
+
+class DouyuLive(Live):
+    api_url = 'http://open.douyucdn.cn/api/RoomApi/room'
+
+    @property
+    def platform():
+        return 'douyu'
+    
+    @classmethod
+    async def get_name_from_room(cls, room_id: int) -> dict:
+        headers = {
+            'Referer': f'https://www.douyu.com/{room_id}',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        }
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(f'{cls.api_url}/{room_id}', headers=headers) as resp:
+                    if resp.status == 200:
+                        json_dic = await resp.json()
+                        return json_dic['data']['owner_name']
+                    else:
+                        logger.warning(f'未能成功获取主播名')
+                        return 'unknown'
+            except Exception as e:
+                logger.exception(e)
+                return 'unknown'
+
+    @classmethod
+    async def _get_douyu_live_info(cls, room_id: int, use_proxy=False) -> dict:
+        proxy = 'http://' + get_proxy() if use_proxy else None
+        headers = {
+            'Referer': f'https://www.douyu.com/{room_id}',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        }
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(f'{cls.api_url}/{room_id}', headers=headers, proxy=proxy) as resp:
+                    if resp.status == 200:
+                        json_dic = await resp.json()
+                        return json_dic
+                    else:
+                        logger.warning(f'访问斗鱼直播发生错误，错误码{resp.status}')
+            except Exception as e:
+                #logger.exception(e)
+                return {}
+
+    async def check_update(self):
+        # 根据直播时间检查所有直播间是否更新
+        subs: List[SubscribedLive] = SubscribedLive.select().where(SubscribedLive.platform == 'douyu')
+        if not subs:
+            logger.info('检查直播： 当前未订阅直播')
+            return
+        for sub in subs:
+            last_date = sub.date
+            resp = await self._get_douyu_live_info(sub.room_id, use_proxy=True)
+            if not resp:
+                logger.warning(f'检查斗鱼直播间{sub.room_id}出错')
+                continue
+            data = resp['data']
+            if last_date != data['start_time'] and data['room_status'] == '1':
+                logger.info(f'检测到斗鱼直播间{sub.room_id}更新')
+                sub.date = data['start_time']
+                sub.save()
+                await self.notice(sub, data['room_name'], f'https://www.douyu.com/{data["room_id"]}')
+    
+    @classmethod
+    async def check_room_exists(cls, room_id: int) -> bool:
+        data = await cls._get_douyu_live_info(room_id)
+        return data 
+        
+
+            
+
+            
+
+
+        
+        
+
+    
+
+
+
+
+        
+
+
+
