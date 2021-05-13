cb885a9182200ab9afc5247d0da5f105e807b035
shewinder <2813349544@qq.com>
2021-05-13 22:55:36 +0800

    添加接头资源的版本管理

--- hoshino/base/help.py
+++ hoshino/base/help.py
@@ -4,4 +4,4 @@ help = on_command('help', aliases={'帮助', '机器人帮助', '使用手册'})
 
 @help.handle()
 async def _(bot, event):
-    await help.send('http://bot.shewinder.win/help')
+    await help.send('http://bot.shewinder.win')
--- hoshino/modules/entertainment/conhead/config.py
+++ hoshino/modules/entertainment/conhead/config.py
@@ -3,8 +3,8 @@ from os import path
 from hoshino.pluginconfig import BaseConfig, PluginConfig
 
 class Config(BaseConfig):
-    CLIENT_ID = ''
-    CLIENT_SECRET = ''
+    CLIENT_ID = 'dkGndecuTLxEq7NvePAz0eQk'
+    CLIENT_SECRET = 'MweFIf6DeqGANH7azo0gWAFjmtAONN1B'
     DAILY_MAX_NUM = 5
     DEFAULT_MODE = 2 # 1使用百度api， 2使用opencv
 
