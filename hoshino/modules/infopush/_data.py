94f89b2e3189b7a14a2559ed5730b143862c4fd3
shewinder <2813349544@qq.com>
2022-06-05 13:44:43 +0800

    数据库采用自增主键

--- hoshino/modules/infopush/_data.py
+++ hoshino/modules/infopush/_data.py
@@ -7,6 +7,7 @@ db_path = db_dir.joinpath('infopush.db')
 db = pw.SqliteDatabase(db_path)
 
 class SubscribeRecord(pw.Model):
+    id = pw.AutoField()
     url = pw.TextField()
     checker = pw.TextField()
     remark = pw.TextField(default="")
@@ -16,15 +17,17 @@ class SubscribeRecord(pw.Model):
 
     class Meta:
         database = db
-        primary_key = pw.CompositeKey('url', 'creator', 'group')
-
-def query_records():
-    return SubscribeRecord.select()
+        indexes = (
+            (('url', 'group', 'creator'), True),
+        )
 
 if not os.path.exists(db_path):
     db.connect()
     db.create_tables([SubscribeRecord])
     db.close()
+
+def query_records():
+    return SubscribeRecord.select()
 
 class ExistsException(Exception):
     pass
